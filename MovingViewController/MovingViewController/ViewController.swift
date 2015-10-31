311a1c1aeb69cb7ca0c8b8a61f3093147117b805
YTsuru02 <Y_Tsurugai@tsurubaiyasuo-no-MacBook-Air.local>
2015-10-31 11:39:28 +0900

    MovingViewControllerという名前のswiftのプロジェクトを作成。

---
+++ MovingViewController/MovingViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovingViewController
+//
+//  Created by 鶴貝康男 on 2015/10/31.
+//  Copyright (c) 2015年 鶴貝康男. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
